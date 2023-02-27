c250048da9f5f3ebf7d6ba4abd8fcc36c2aa6133
Akiyama <Akiyama.Minoru@otsuka.jp>
2023-02-27 14:31:58 +0900

    add main

--- show.py
+++ show.py
@@ -1,43 +1,13 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "a56eee08",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "#!/usr/bin/env python3\n",
-    "# -*- coding: utf-8 -*-\n",
-    "\n",
-    "import random\n",
-    "\n",
-    "with open(\"./hyakunin.txt\", encoding=\"utf-8\") as f:\n",
-    "    wakas = [s.strip() for s in f.readlines()]\n",
-    "\n",
-    "print(wakas[random.randrange(len(wakas))])"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.9.13"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import random
+
+def main():
+
+    with open("./hyakunin.txt", encoding="utf-8") as f:
+        wakas = [s.strip() for s in f.readlines()]
+        print(wakas[random.randrange(len(wakas))])
+
+if __name__ == '__main__':
+    main()   
